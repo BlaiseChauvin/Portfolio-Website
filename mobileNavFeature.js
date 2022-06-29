d05cfaba0c3f2e9eae232756778b47d730214f00
Blaise Chauvin <blaisechauvin@gmail.com>
2022-06-29 14:09:05 -0500

    Add Curtin Menu, fix colors

--- mobileNavFeature.js
+++ mobileNavFeature.js
@@ -1,13 +1,10 @@
 /* Open when someone clicks on the span element */
-function openNav() {
-    document.getElementById("navBar").style.display = "block";
-    document.getElementsById("closebtn").style.display = "block";
-    document.getElementsById("openbtn").style.display = "none";
-  }
-  
-  /* Close when someone clicks on the "x" symbol inside the overlay */
-  function closeNav() {
-    document.getElementById("navBar").style.display = "none";
-    document.getElementsByClassName("closebtn").style.display = "none";
-    
-  }
+let openNav = () => {
+  document.getElementById("myNav").style.width = "100%";
+}
+
+
+/* Close when someone clicks on the "x" symbol inside the overlay */
+let closeNav = () => {
+  document.getElementById("myNav").style.width = "0%";
+}
